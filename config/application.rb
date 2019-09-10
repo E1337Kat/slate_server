0629ea4c54a1f3597d5ea24367372697e9c04ff1
Ellie Peterson <ekp2h92@gmail.com>
2019-09-10 11:42:17 -0400

    Update Gems

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-require File.expand_path('boot', __dir__)
+
+require_relative 'boot'
 
 # To remove DB, we should not require all of rails.
 # require 'rails/all'
@@ -27,26 +28,28 @@ require 'active_support/logger'
 Bundler.require(*Rails.groups)
 
 # Core application class
-class SlateServer::Application < Rails::Application
-  # Settings in config/environments/* take precedence over those specified here.
-  # Application configuration should go into files in config/initializers
-  # -- all .rb files in that directory are automatically loaded.
+module SlateServer
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
 
-  # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-  # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-  # config.time_zone = 'Central Time (US & Canada)'
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
 
-  # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-  # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-  # config.i18n.default_locale = :de
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
 
-  # Do not swallow errors in after_commit/after_rollback callbacks.
-  # config.active_record.raise_in_transactional_callbacks = true
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    # config.active_record.raise_in_transactional_callbacks = true
 
-  config.eager_load_paths << Rails.root.join('lib')
-  config.data = config_for(:data)
+    config.eager_load_paths << Rails.root.join('lib')
+    config.data = config_for(:data)
 
-  config.logger = Logger.new(STDOUT)
+    config.logger = Logger.new(STDOUT)
 
-  paths['build'] = 'build'
+    paths['build'] = 'build'
+  end
 end
